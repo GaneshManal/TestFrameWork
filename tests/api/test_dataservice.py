0559aaf6f747a81d16c52d67394fa0a152c789f9
Piyush Pote <pote_p@India.XoriantCorp.com>
2018-06-29 13:25:49 +0530

    Applied proxy to dm, applied skip test to dataservice

--- tests/api/test_dataservice.py
+++ tests/api/test_dataservice.py
@@ -1,5 +1,6 @@
 import requests
-from util.pnda import read_dataservice_endpoints
+from util import pnda
+from util.pynose import skip, skipIf
 
 
 
@@ -8,13 +9,15 @@ class TestDataservice(object):
     _proxies = None
 
     def __init__(self):
-        self._edge_ip, self._dataservice_port, self._proxies = read_dataservice_endpoints()
+        self._edge_ip, self._dataservice_port, self._proxies = pnda.read_dataservice_endpoints()
 
+    @skipIf(not pnda.cluster_reachable, "cluster Not reachable")
     def test_list_datasets(self):
         response = requests.get("http://%s:%d/api/v1/datasets" % (self._edge_ip, self._dataservice_port),
                                 proxies=self._proxies)
         # response = requests.get("http://%s:%d/environment/endpoints" % (self._edge_ip, self._dm_port), proxies=self._proxies)
         assert response.status_code == 200
 
+    @skipIf(not pnda.cluster_reachable, "cluster Not reachable")
     def test_list_deployed_packages(self):
         assert 'b' == 'b'
