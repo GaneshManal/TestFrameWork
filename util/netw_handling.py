b8da18a0b2127b3f48e83f23669599b5cac777ec
Piyush Pote <pote_p@India.XoriantCorp.com>
2018-07-11 12:52:50 +0530

    SCP and executing command on remote server added

--- util/netw_handling.py
+++ util/netw_handling.py
@@ -1,23 +1,49 @@
 import paramiko
+
+
 class RemoteServer(object):
-    _node_ip = None
-    _pem_file_location = None
+    """
+    Class to connect to a remote server
+    """
 
     def __init__(self, node_ip, pem_file_location):
         self._node_ip = node_ip
         self._pem_file_location = pem_file_location
 
-    def exec_commands(self, c, cmd):
+    @staticmethod
+    def scp_to_node(client, src, dest):
+        """
+        Copies one file from local to remote destination
+        :param client: client object
+        :param src: source file location
+        :param dest: destination location
+        :return: None
+        """
+        ftp_client = client.open_sftp()
+        ftp_client.put(src, dest)
+        ftp_client.close()
+
+    @staticmethod
+    def exec_commands(client, cmd):
+        """
+        Executes a list of commands on the remote server
+        :param client: client object
+        :param cmd: list of commands
+        :return: None
+        """
         for command in cmd:
             print "Executing {}".format(command)
-            stdin , stdout, stderr = c.exec_command(command)
+            stdin, stdout, stderr = client.exec_command(command)
             print stdout.read()
-            print( "Errors")
-            print stderr.read()
 
-    def parse_node_details(self, node_details):
-        node_list = node_details.split("\n")
-        node_list = node_list[1: len(node_list)-1]
+    @staticmethod
+    def parse_node_details(node_details):
+        """
+        Parse the node details
+        :param node_details: output of the consul commands
+        :return: dict of node_details, dns as key and ip address as value
+        """
+        node_list = node_details.split("\n")[1:-1]
         node_dict = {}
         for node_str in node_list:
             node = node_str.split()
@@ -27,43 +53,34 @@ class RemoteServer(object):
         return node_dict
 
     def get_all_nodes(self, client):
-        stdin, stdout, stderr = client.exec_command("consul members --http-addr http://$(hostname --ip-address):8500")
+        """
+        gets the ip address and dns of all the nodes
+        :param client: client object
+        :return: dict of node_details, dns as key and ip address as value
+        """
+        stdin, stdout, stderr = client.exec_command(
+            "consul members --http-addr http://$(hostname --ip-address):8500")
         node_dict = self.parse_node_details(stdout.read())
         return node_dict
 
-    def copy_file(self, src, target, filename):
-        pass
-
     def connect(self):
+        """
+        connects to a remote server
+        :return: connected client object
+        """
         key = paramiko.RSAKey.from_private_key_file(self._pem_file_location)
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        # print "connecting"
-        client.connect(hostname = self._node_ip, username = "ec2-user", pkey = key)
-        # self.exec_commands(client, ["consul members --http-addr http://$(hostname --ip-address):8500"])
+        client.connect(hostname=self._node_ip, username="ec2-user", pkey=key)
         print "connection successfull"
         return client
 
-    def parse_output(self):
-        pass
-
-    def close_connection(self, client):
+    @staticmethod
+    def close_connection(client):
+        """
+        closes the connection to the remote server
+        :param client: client object
+        :return: None
+        """
         client.close()
         print "connection closed successfully"
-
-
-# import paramiko
-# k = paramiko.RSAKey.from_private_key_file("/Users/whatever/Downloads/mykey.pem")
-# c = paramiko.SSHClient()
-# c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-# print "connecting"
-# c.connect( hostname = "www.acme.com", username = "ubuntu", pkey = k )
-# print "connected"
-# commands = [ "/home/ubuntu/firstscript.sh", "/home/ubuntu/secondscript.sh" ]
-# for command in commands:
-# 	print "Executing {}".format( command )
-# 	stdin , stdout, stderr = c.exec_command(command)
-# 	print stdout.read()
-# 	print( "Errors")
-# 	print stderr.read()
-# c.close()
